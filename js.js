59c5cff774a6f3c8a6a9889398e8090142284296
SaborataRekaReka <122129669+SaborataRekaReka@users.noreply.github.com>
2023-01-07 03:49:59 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,29 @@
 let Stars = document.querySelector('.Stars');
-let Dollar ="";
+let StarCheck = [];
+let Test ="";
 
 function myFunction() {
-  // var x = Stars.value;
-  // Dollar.innerHTML = "You wrote: " + x;
+Test = Stars.value.toString(10);
+Test = Test.slice(-1);
+StarCheck.push(Test);
+if (StarCheck[0] !== "⭐") {
+  console.log(StarCheck[0]);
+  console.log("НАЕБКА");
+  StarCheck.pop();
+  Stars.value="";
+  document.getElementById("demo").innerHTML  = "МЫ ПРИНИМАЕМ ТОЛЬКО ЗВЕЗДОЧКИ!";
+} else{
+  console.log("НЕНАЕБКА");
+  StarCheck.pop();
+  document.getElementById("demo").innerHTML  = "";
 
-let Dollars = Stars.value;
-Dollars = Dollars.toString(10);
-Dollars =  Dollars.length;
+  let Dollars = Stars.value;
+  Dollars = Dollars.toString(10);
+  Dollars =  Dollars.length;
 
-console.log("Я долары"+ Dollars);
-for (let i = 0; i < Dollars; i++) {
-  Dollar  = Dollar + "	💲"
-  };
-  document.getElementById("demo").innerHTML = Dollar;
+  for (let i = 0; i < Dollars; i++) {
+    document.getElementById("demo").innerHTML  = document.getElementById("demo").innerHTML  + "💲"
+    };
+
+  }
 }
