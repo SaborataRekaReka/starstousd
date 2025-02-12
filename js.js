13474ae41a44681f632f36867a56ee624461e54b
SaborataRekaReka <122129669+SaborataRekaReka@users.noreply.github.com>
2025-02-12 16:26:58 +0400

    Update js.js

--- js.js
+++ js.js
@@ -3,27 +3,26 @@ let StarCheck = [];
 let Test ="";
 
 function myFunction() {
-Test = Stars.value.toString(10);
-Test = Test.slice(-1);
-StarCheck.push(Test);
-if (StarCheck[0] !== "⭐") {
-  console.log(StarCheck[0]);
-  console.log("НАЕБКА");
-  StarCheck.pop();
-  Stars.value="";
-  document.getElementById("demo").innerHTML  = "МЫ ПРИНИМАЕМ ТОЛЬКО ЗВЕЗДОЧКИ!";
-} else{
-  console.log("НЕНАЕБКА");
-  StarCheck.pop();
-  document.getElementById("demo").innerHTML  = "";
+  Test = Stars.value.toString(10);
+  Test = Test.slice(-1);
+  StarCheck.push(Test);
+  if (StarCheck[0] !== "⭐" && StarCheck[0] !== "⭐️") {
+    console.log(StarCheck[0]);
+    console.log("НАЕБКА");
+    StarCheck.pop();
+    Stars.value="";
+    document.getElementById("demo").innerHTML  = "МЫ ПРИНИМАЕМ ТОЛЬКО ЗВЕЗДОЧКИ!";
+  } else{
+    console.log("НЕНАЕБКА");
+    StarCheck.pop();
+    document.getElementById("demo").innerHTML  = "";
 
-  let Dollars = Stars.value;
-  Dollars = Dollars.toString(10);
-  Dollars =  Dollars.length;
+    let Dollars = Stars.value;
+    Dollars = Dollars.toString(10);
+    Dollars =  Dollars.length;
 
-  for (let i = 0; i < Dollars; i++) {
-    document.getElementById("demo").innerHTML  = document.getElementById("demo").innerHTML  + "💲"
-    };
-
+    for (let i = 0; i < Dollars; i++) {
+      document.getElementById("demo").innerHTML  = document.getElementById("demo").innerHTML  + "💲";
+    }
   }
 }
